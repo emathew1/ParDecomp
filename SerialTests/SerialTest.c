17bba9d71e5a1128b5c3c4211bcbdb6ed808116a
emathew1 <emathew1@nd.edu>
2015-05-17 17:28:44 -0400

    Starting to go through and improve memory allocation and I/O stuff...

--- SerialTests/SerialTest.c
+++ SerialTests/SerialTest.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 #include <lapacke.h>
 #include "lapacke_example_aux.h"
 
@@ -14,7 +15,7 @@ int main(int argc, char *argv[])
      return(-1);
    }else{
      fread(mnIn, sizeof(int), 2, fid);
-     printf("Reading in Data file of size %d by %d\n", mnIn[0], mnIn[1]);
+     printf("Data dimensions - %d by %d\n", mnIn[0], mnIn[1]);
      fclose(fid);
    }
   
@@ -32,8 +33,30 @@ int main(int argc, char *argv[])
      return(-1);
    }
    printf("A is allocated!\n");
-   double R[m*n];
-   double tau[n];
+   double *R;
+   if(NULL==(R = malloc(m*n*sizeof(double)))){
+    printf("malloc of R failed\n");
+    return(-1);
+   }
+   printf("R is allocated!\n");
+   double *tau;
+   if(NULL==(tau = malloc(n*sizeof(double)))){
+     printf("malloc of Tau failed\n");
+     return(-1);
+   }
+   printf("tau is allocated!\n");
+
+   //Read in data file
+   fid = fopen("TestIn.dat", "r");
+   if(fid ==NULL){
+     printf("TestIn.dat file could not open!\n");
+     return(-1);
+   }else{
+     fread(A, sizeof(double), m*n, fid);
+     printf("Reading in Data file of size %d by %d\n", mnIn[0], mnIn[1]);
+     fclose(fid);
+   }
+   printf("Finished reading in the %d data points!\n", m*n);
 
 
    //Nice if we have smaller matrices
@@ -41,48 +64,42 @@ int main(int argc, char *argv[])
    //printf( "\n" );
 
    //Solving the Double GEneral matrix QR decomposition
+
+   printf("Solving the QR decomp...");
    info = LAPACKE_dgeqrf(LAPACK_COL_MAJOR, m, n, A, lda, tau);
+   printf("done!\n");
 
    //Use this to make a smaller upper triangular matrix
-   int upperTriSize = n*(n+1)/2;
-   double R_upperTri[upperTriSize];
+   //int upperTriSize = n*(n+1)/2;
+   //double R_upperTri[upperTriSize];
    
    //Copy over the data from the solution matrix A to the upper tri R
+   printf("Memcpy results to R matrix...");
    memcpy(R, A, sizeof(double)*m*n);
+   printf("done!\n");
 
-
-   //Nice if we have smaller matrices
-   //print_matrix_colmajor("R Solution", m, n, A, lda);
-   //printf( "\n" );
-
-   //Nice if we have smaller matrices
-   //print_matrix_colmajor("R Solution Copy", m, n, R, lda);
-   //printf( "\n" );
-
-   //This will give us the Q of the QR decomp from the previous Lapacke function
+   printf("Getting the Q data from the QGEQRF output matrix...");
    info = LAPACKE_dorgqr(LAPACK_COL_MAJOR, m, n, n, A, lda, tau);
-   //print_matrix_colmajor("Q Solution", m, n, A, lda);
-   //printf( "\n" );
+   printf("done!\n");
 
    //Lapacke svd function
-
    //Allocating Solution Matrices
    double S[n], U[m*m], Vt[n*n];
    double superb[n-1];
    info = LAPACKE_dgesvd(LAPACK_COL_MAJOR, 'A', 'A', m, n, R, m, S, U, m, Vt, n, superb);
 
-   //print_matrix_colmajor("U", m, m, U, lda);
-   //printf( "\n" );
+   //TODO: Write function to compare with MATLAB solutions to do error checking
 
-   //print_matrix_colmajor("S", n, 1, S, 1);
-   //printf( "\n" );
+   //TODO: Need to deallocate solution matrices
 
-   //print_matrix_colmajor("Vt", n, n, Vt, n);
-   //printf( "\n" );
+   return(0);
+}
 
-   //Write function to compare with MATLAB solutions to do error checking
-
-   //Need to deallocate solution matrices
-
-   exit(0);
+int allocateData(double *dataPtr, int length, char* description){
+   if(NULL==(dataPtr = malloc(length*sizeof(double)))){
+     printf("malloc of %s failed\n", description);
+     return(-1);
+   }
+   printf("%s is allocated!\n", description);
+   return(0);
 }  
